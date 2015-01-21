f205d561975be1d3472f5e6ad266730ee1e0312a
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-21 17:15:40 -0500

    use assert

--- test.js
+++ test.js
@@ -1,20 +1,20 @@
 /**
  * longest <https://github.com/jonschlinkert/longest>
  *
- * Copyright (c) 2014 Jon Schlinkert
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT license.
  */
 
-var should = require('should');
+var assert = require('assert');
 var expand = require('fill-range');
 var longest = require('./');
 
 describe('longest', function() {
   it('Should return the longest item in the array', function() {
-    longest(['a', 'abcde', 'abc']).should.eql('abcde');
-    longest(['a', 'abcde', 'abc']).length.should.eql(5);
-    longest(expand('0', '10')).should.eql('10');
-    longest(expand('0', '100')).should.eql('100');
-    longest(expand('0', '1000')).should.eql('1000');
+    assert.equal(longest(['a', 'abcde', 'abc']), 'abcde');
+    assert.equal(longest(['a', 'abcde', 'abc']).length, 5);
+    assert.equal(longest(expand('0', '10')), '10');
+    assert.equal(longest(expand('0', '100')), '100');
+    assert.equal(longest(expand('0', '1000')), '1000');
   });
 });
